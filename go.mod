715a0875c1d4b9df93d83ef9db5c30cf54169a12
Omsurvase24 <omsurvase24@gmail.com>
2024-10-01 19:14:01 +0530

    http req

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/Omsurvase24/rssagg
 
 go 1.23.1
 
-require github.com/joho/godotenv v1.5.1
+require (
+	github.com/go-chi/chi v1.5.5
+	github.com/joho/godotenv v1.5.1
+)
--- main.go
+++ main.go
@@ -3,19 +3,33 @@ package main
 import (
 	"fmt"
 	"log"
+	"net/http"
 	"os"
 
+	"github.com/go-chi/chi"
 	"github.com/joho/godotenv"
 )
 
 func main() {
-	fmt.Println("Hello")
 
 	godotenv.Load(".env")
 
 	portString := os.Getenv("PORT")
 	if portString == "" {
 		log.Fatal("Port is not found in the env")
+	}
+
+	router := chi.NewRouter()
+
+	srv := &http.Server{
+		Handler: router,
+		Addr:    ":" + portString,
+	}
+
+	log.Printf("Server starting on port %v", portString)
+	err := srv.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
 	}
 
 	fmt.Println("Port:", portString)
