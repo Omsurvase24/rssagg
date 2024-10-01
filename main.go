c93560ca962b4a74e654b86827c9bd3556101603
Omsurvase24 <omsurvase24@gmail.com>
2024-10-01 23:26:11 +0530

    handle get

--- main.go
+++ main.go
@@ -31,6 +31,11 @@ func main() {
 		MaxAge:           300,
 	}))
 
+	v1Router := chi.NewRouter()
+	v1Router.Get("/healthz", handlerReadiness)
+
+	router.Mount("/v1", v1Router)
+
 	srv := &http.Server{
 		Handler: router,
 		Addr:    ":" + portString,
