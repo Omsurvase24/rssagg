bbc8d872fdecff0cff2ce57b76ebb3ed9f3a7c3f
Omsurvase24 <omsurvase24@gmail.com>
2024-10-04 19:15:58 +0530

    get user by api

--- handler_user.go
+++ handler_user.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 	"time"
 
+	"github.com/Omsurvase24/rssagg/internal/auth"
 	"github.com/Omsurvase24/rssagg/internal/database"
 	"github.com/google/uuid"
 )
@@ -31,6 +32,22 @@ func (apiCfg *apiConfig) handlerCreateUser(w http.ResponseWriter, r *http.Reques
 	})
 	if err != nil {
 		respondWithError(w, 400, fmt.Sprintf("Couldn't create user: %s", err))
+		return
+	}
+
+	respondWithJSON(w, 201, databaseUserToUser(user))
+}
+
+func (apiCfg *apiConfig) handlerGetUser(w http.ResponseWriter, r *http.Request) {
+	apiKey, err := auth.GetAPIKey(r.Header)
+	if err != nil {
+		respondWithError(w, 403, fmt.Sprintf("Auth error %v", err))
+		return
+	}
+
+	user, err := apiCfg.DB.GetUserByAPIKey(r.Context(), apiKey)
+	if err != nil {
+		respondWithError(w, 400, fmt.Sprintf("Couldn't get user: %v", err))
 		return
 	}
 
