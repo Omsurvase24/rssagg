72808688ecf055afc5a72ea71b965661fd065aa5
Omsurvase24 <omsurvase24@gmail.com>
2024-10-05 19:27:13 +0530

    create feed

--- main.go
+++ main.go
@@ -58,6 +58,7 @@ func main() {
 	v1Router.Get("/err", handlerErr)
 	v1Router.Post("/users", apiCfg.handlerCreateUser)
 	v1Router.Get("/users", apiCfg.middlewareAuth(apiCfg.handlerGetUser))
+	v1Router.Post("/feeds", apiCfg.middlewareAuth(apiCfg.handlerCreateFeed))
 
 	router.Mount("/v1", v1Router)
 
