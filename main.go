bde2e1fa9a641affef2b397cad7eb6c1e7f6ebe8
Omsurvase24 <omsurvase24@gmail.com>
2024-10-06 20:21:36 +0530

    feed last fetched

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"time"
 
 	"github.com/Omsurvase24/rssagg/internal/database"
 	"github.com/go-chi/chi"
@@ -38,9 +39,12 @@ func main() {
 		log.Fatal("Can't connect to database")
 	}
 
+	db := database.New(conn)
 	apiCfg := apiConfig{
-		DB: database.New(conn),
+		DB: db,
 	}
+
+	go startScraping(db, 10, time.Minute)
 
 	router := chi.NewRouter()
 
